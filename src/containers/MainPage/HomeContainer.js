6ba4cd51ffc8310bb8d849e36c9f007ea3e075d1
admin <tranngocdung.201194@gmail.com>
2018-09-14 09:51:40 +0700

    stage change

---
+++ src/containers/MainPage/HomeContainer.js
@@ -0,0 +1,13 @@
+import {
+    connect,
+} from 'react-redux';
+
+import HomeView from './HomeView';
+
+const mapStateToProps = () => ({
+
+});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeView);
