193374ec855275200e6e4ce97103743689e6e706
Dung Trann <dung.trann@groovetechnology.com>
2018-10-03 15:42:42 +0700

    feature: molecur Avatar

--- src/ui/Avatar.js
+++ src/ui/Avatar.js
@@ -2,27 +2,73 @@ import React from 'react'
 import _ from 'lodash'
 import PropTypes from 'prop-types'
 import Img from 'react-image'
+import pose from 'react-pose'
 import {
     colors,
 } from '@internal/styles'
-import { AppConfig } from '@internal/constants';
+import { AppConfig } from '@internal/constants'
+
+const defaultContainerStyle = {
+    backgroundColor: colors.white,
+    borderRadius: '50%',
+    overflow: 'hidden',
+    margin: 'auto',
+}
 
 const defaultStyle = {
-    backgroundColor: colors.white,
-    padding: 2,
     borderRadius: '50%',
+    overflow: 'hidden',
+    marginLeft: 5,
+    marginTop: 5,
 }
+
+const HoverImg = pose.div({
+    hoverable: true,
+    init: {
+        scale: 1,
+    },
+    hover: {
+        scale: 1.2,
+    }
+})
 
 const Avatar = ({
     imageUrl,
     size,
 }) => {
-    const newStyle = _.merge(defaultStyle, size)
+    const newContainerStyle = _.merge(defaultContainerStyle, size)
+    const newStyle = _.merge(defaultStyle, {
+        width: size.width - 10,
+        height: size.height - 10,
+    })
+    const imgStyle = size.height > size.width
+        ? {
+            width: size.width,
+            marginTop: -1 * size.height / 3,
+            zIndex: 2,
+        } : {
+            height: size.height,
+            marginLeft: -1 * size.width / 3,
+            zIndex: 2,
+        }
     return (
-        <Img
-            src={imageUrl}
-            style={newStyle}
-        />
+        <div
+            style={newContainerStyle}
+        >
+            <div
+                style={newStyle}
+            >
+                <Img
+                    src={imageUrl}
+                    style={imgStyle}
+                    container={chil => (
+                        <HoverImg className='imageHoverZoom' >
+                            {chil}
+                        </HoverImg>
+                    )}
+                />
+            </div>
+        </div>
     )
 }
 
@@ -43,8 +89,8 @@ Avatar.propTypes = {
 Avatar.defaultProps = {
     imageUrl: AppConfig.img.avatar,
     size: {
-        width: 70,
-        height: 70,
+        width: 190,
+        height: 190,
     }
 }
 
